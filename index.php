50000117109c361f9f8e739b40c37e7bd0412706
Cesar dos Santos de Almeida <cesarsantosss499@gmail.com>
2021-11-20 21:00:39 -0300

    Criação: Comentarios para orientação do desenvolvedor

--- index.php
+++ index.php
@@ -1,20 +1,23 @@
 <?php
-
+// Chamando o autoload do composer
 require_once __DIR__ . "/vendor/autoload.php";
-
+//criando um array vazio
 $result = [];
 
 use Rota\Rota;
 use Services\Usuarios;
 use Utility\Json;
 
+// verificando ser o $_GET não esta vazio
 if(!empty($_GET))
 {
+    // chamando as funções para a validação e criação da rota
     $url = Rota::getUrl();
     $dados = Rota::getRota($url);
 
     if(Rota::validaRota($dados['rota']) == false)
     {
+        // Cria as informações para serem exibidas ao usuario
         http_response_code(400);
         $result['status'] = STATUS_ERROR;
         $result['metodo'] = $_SERVER['REQUEST_METHOD'];
@@ -24,6 +27,7 @@ if(!empty($_GET))
 
     if(Rota::validaMetodo($dados['metodo']) == false)
     {
+        // Cria as informações para serem exibidas ao usuario
         http_response_code(400);
         $result['status'] = STATUS_ERROR;
         $result['metodo'] = $_SERVER['REQUEST_METHOD'];
@@ -31,59 +35,66 @@ if(!empty($_GET))
         exit;
     }
 
-
+    // Verificando ser a rota é permitida
     if($dados['rota'] == "usuarios")
     {
-
+        //Instanciando a class usuarios
         $services = new Usuarios;
-
+        // Verificando o metodo
         switch ($dados['metodo']) 
         {
             case 'POST':
-
+                // Sentando as informações
                 $services->nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
                 $services->idade = filter_input(INPUT_POST, 'idade', FILTER_SANITIZE_NUMBER_INT);
                 $services->sexo = filter_input(INPUT_POST, 'sexo', FILTER_SANITIZE_SPECIAL_CHARS);
                 $services->email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
                 $services->senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_SPECIAL_CHARS);
-
+                //chamando a função 
                 $resposta = $services->POST();
-
+                //pegando o resultado
                 $result = $resposta;
 
                 break;
 
             case 'GET':
-                
+                // vereficando ser o identifacador não é vazio
                 if(!empty($dados['identificador']))
                 {
+                    // Sentando as informações
                     $services->idUsuario = filter_var($dados['identificador']);
+                     //chamando a função 
                     $result = $services->GETID();
                 }
                 else
                 {
+                    // chamando a função 
                     $result = $services->GET();
                 }
 
                 break;
 
             case 'PUT':
+                // Armazenando as informações
                 parse_str(file_get_contents("php://input"),$putData);
                 
+                // Vereficando ser foi enviado todas as informações
                 if (!empty($putData['id']) and !empty($putData['nome']) and !empty($putData['idade']) and !empty($putData['sexo']) and !empty($putData['email']) and !empty($putData['senha'])) 
                 {
-
+                    // Sentando as informações
                     $services->idUsuario = filter_var($putData['id'], FILTER_SANITIZE_NUMBER_INT);
                     $services->nome = filter_var($putData['nome'], FILTER_SANITIZE_SPECIAL_CHARS);
                     $services->idade = filter_var($putData['idade'] , FILTER_SANITIZE_NUMBER_INT);
                     $services->sexo = filter_var($putData['sexo'], FILTER_SANITIZE_SPECIAL_CHARS);
                     $services->email = filter_var($putData['email'], FILTER_SANITIZE_EMAIL);
                     $services->senha = filter_var($putData['senha'], FILTER_SANITIZE_SPECIAL_CHARS);
+                    //chamando a função 
                     $result = $services->PUT();
 
                 }
                 else
                 {
+                    // Cria as informações para serem exibidas ao usuario
                     http_response_code(404);
                     $result['status'] = STATUS_ERROR;
                     $result['metodo'] = $_SERVER['REQUEST_METHOD'];
@@ -93,12 +104,14 @@ if(!empty($_GET))
                 break;
 
             case 'DELETE':
+                // Sentando as informações
                 $services->idUsuario= $dados['identificador'];
-                $resposta = $services->DELETE();
-                $result = $resposta;
+                //chamando a função 
+                $result = $services->DELETE();
                 break;
             
             default:
+            // Cria as informações para serem exibidas ao usuario
                 http_response_code(400);
                 $result['status'] = STATUS_ERROR;
                 $result['metodo'] = $_SERVER['REQUEST_METHOD'];
@@ -108,6 +121,7 @@ if(!empty($_GET))
     }
     elseif (empty($dados))
     {
+        // Cria as informações para serem exibidas ao usuario
         http_response_code(400);
         $result['status'] = "ERROR";
         $result['metodo'] = $_SERVER['REQUEST_METHOD'];
@@ -115,6 +129,7 @@ if(!empty($_GET))
     }
     else
     {
+        // Cria as informações para serem exibidas ao usuario
         http_response_code(400);
         $result['status'] = "ERROR";
         $result['metodo'] = $_SERVER['REQUEST_METHOD'];
@@ -124,12 +139,13 @@ if(!empty($_GET))
 }
 else
 {
+    // Cria as informações para serem exibidas ao usuario
     http_response_code(400);
     $result['status'] = "Infor";
     $result['metodo'] = $_SERVER['REQUEST_METHOD'];
     $result['result'] = BASEURL . "usuarios";
 }
 
-
+// Exibindo as informações em json
 Json::dadosJson($result);
 
